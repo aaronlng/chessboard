40509a8877d1220ebdfdcc6f6eab90d3aa1c8669
Aaron Long <aaronlng05@gmail.com>
2020-03-30 11:41:51 -0400

    solved js file

--- chessboard.js
+++ chessboard.js
@@ -3,5 +3,14 @@ let size = 8;
 let board = "";
 
 for (let i = 0; i < size; i++){
-    console.log(i);
+    for (let j = 0; j < size; j++){
+        if ((i + j) % 2 == 0){
+            board += " ";
+        } else {
+            board += "#";
+        }
+    }
+    board += "\n";
 }
+
+console.log(board);
